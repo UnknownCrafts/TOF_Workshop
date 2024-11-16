70b291ec0dce9681f725061b1bbb0f124efd68aa
Surya Vasudev <suryavasudev05@gmail.com>
2024-11-16 16:49:33 -0500

    QOL changes in pointcloud_viewer.py

--- pointcloud_viewer.py
+++ pointcloud_viewer.py
@@ -1,6 +1,21 @@
 import open3d as o3d
 import numpy as np
 
-pcd = o3d.io.read_point_cloud("sample-data/3mm-distance.pcd")
+input = input("Enter a number from 1 to 7: ")
+
+if input == "2":
+    pcd = o3d.io.read_point_cloud("sample-data/close.pcd")
+elif input == "3":
+    pcd = o3d.io.read_point_cloud("sample-data/hand-close.pcd")
+elif input == "4":
+    pcd = o3d.io.read_point_cloud("sample-data/hand-touching.pcd")
+elif input == "5":
+    pcd = o3d.io.read_point_cloud("sample-data/no-obj.pcd")
+elif input == "6":
+    pcd = o3d.io.read_point_cloud("sample-data/superclose.pcd")
+elif input == "7":
+    pcd = o3d.io.read_point_cloud("sample-data/touching.pcd")
+else:
+    pcd = o3d.io.read_point_cloud("sample-data/3mm-distance.pcd")
 
 o3d.visualization.draw_geometries([pcd])
